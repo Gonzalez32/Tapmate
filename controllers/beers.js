aa3774aa5c2eba04e4013014715d988e6f9558c8
Gonzalez32 <3232rose@gmail.com>
2020-12-17 23:59:04 -0600

    modified views

--- controllers/beers.js
+++ controllers/beers.js
@@ -4,16 +4,30 @@ const axios = require('axios')
 
 module.exports = {
     new: newBeer,
+    index,
     show,
     addToCollection,
     removeFromCollection,
-    index,
 }
 
 function newBeer(req, res) {
     res.render("beers/new", {
         title: "Beer",
         user: req.user,
+    })
+}
+
+function index(req, res) {
+    axios.get(`https://api.punkapi.com/v2/beers/`)
+    .then((resp) => {
+        Beer.find({}, (err, beers) => {
+            // console.log(resp.data);
+            res.render('beers/index', {
+                title: "Beer Collections",
+                user: req.user,
+                beers: resp.data,
+            })
+        })
     })
 }
 
@@ -77,16 +91,3 @@ function removeFromCollection(req, res) {
     })
 }
 
-function index(req, res) {
-    axios.get(`https://api.punkapi.com/v2/beers/`)
-    .then((resp) => {
-        Beer.find({}, (err, beers) => {
-            // console.log(resp.data);
-            res.render('beers/index', {
-                title: "Beer Collections",
-                user: req.user,
-                beers: resp.data,
-            })
-        })
-    })
-}
